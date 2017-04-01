08406e021239b3c3fc0100a756a6e15eb1371d61
Laura van der Stee <lauravanderstee@gmail.com>
2017-04-01 10:15:34 +0200

    Een tweede scherm en een werkende tabel met arrays

--- Emoji dictionary/ViewController.swift
+++ Emoji dictionary/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var mijnTabel: UITableView!
     
+    var emojis = ["😘","🤠","🤗","💄","🐬","🤽‍♀️"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,12 +24,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "😘"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
     
