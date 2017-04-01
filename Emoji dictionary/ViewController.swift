609dddefdb7653e60ec6dce2fb372964a3364a88
Laura van der Stee <lauravanderstee@gmail.com>
2017-04-01 09:33:22 +0200

    Initial Commit

---
+++ Emoji dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji dictionary
+//
+//  Created by Laura van der Stee on 01-04-17.
+//  Copyright © 2017 Laura van der Stee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
